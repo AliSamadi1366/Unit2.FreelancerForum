bb5a3b61c282e9e626b51b643b7b81bb33945f1d
Ali Samadi <alisamadi.096@gmail.com>
2025-02-12 17:07:11 -0600

    wrote a function to calculate the average rate, corrected my mistake in the line 18 const rate to generate right number for rate

--- index.js
+++ index.js
@@ -14,11 +14,19 @@ const NUM_FREELANCERS = 100;
 // Write a function that returns a freelancer object with a randomly name, occupation and rate according to the provided constants
 function makeFreelancer() {
   const name = NAMES[Math.floor(Math.random() * NAMES.length)];
-  const occupation =
-    OCCUPATIONS[Math.floor(Math.random() * OCCUPATIONS.length)];
-  const price = PRICE_RANGE[Math.floor(Math.random() * PRICE_RANGE.length)];
-  const freelancer = { name, occupation, price };
-  return freelancer;
+  const occupation = OCCUPATIONS[Math.floor(Math.random() * OCCUPATIONS.length)];
+  const rate = Math.floor(Math.random() * (PRICE_RANGE.max - PRICE_RANGE.min +1)) + PRICE_RANGE.min;
+  return { name, occupation, rate }; 
 }
 //Initialize a state variable to an array of NUM_FREELANCERS freelancer objects
 let freelancers = Array.from({ length: NUM_FREELANCERS }, makeFreelancer);
+//Write a function that returns the average rate of all freelancers in state.
+function rateAverage(freelancers) {
+  let total = 0;
+  for (const freelancer of freelancers) {
+    
+    total += freelancer.rate;
+  }
+  return total / freelancers.length;
+}
+console.log(rateAverage(freelancers))
