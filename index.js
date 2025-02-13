d728dcdd4e5368b91b310ee18442285e26321589
Ali Samadi <alisamadi.096@gmail.com>
2025-02-13 00:09:49 -0600

    use <tr> element to create a table and correct the FreeLancerRows based on new const, still need to style the table

--- index.js
+++ index.js
@@ -14,9 +14,12 @@ const NUM_FREELANCERS = 100;
 // Write a function that returns a freelancer object with a randomly name, occupation and rate according to the provided constants
 function makeFreelancer() {
   const name = NAMES[Math.floor(Math.random() * NAMES.length)];
-  const occupation = OCCUPATIONS[Math.floor(Math.random() * OCCUPATIONS.length)];
-  const rate = Math.floor(Math.random() * (PRICE_RANGE.max - PRICE_RANGE.min +1)) + PRICE_RANGE.min;
-  return { name, occupation, rate }; 
+  const occupation =
+    OCCUPATIONS[Math.floor(Math.random() * OCCUPATIONS.length)];
+  const rate =
+    Math.floor(Math.random() * (PRICE_RANGE.max - PRICE_RANGE.min + 1)) +
+    PRICE_RANGE.min;
+  return { name, occupation, rate };
 }
 //Initialize a state variable to an array of NUM_FREELANCERS freelancer objects
 let freelancers = Array.from({ length: NUM_FREELANCERS }, makeFreelancer);
@@ -24,7 +27,6 @@ let freelancers = Array.from({ length: NUM_FREELANCERS }, makeFreelancer);
 function rateAverage(freelancers) {
   let total = 0;
   for (const freelancer of freelancers) {
-
     total += freelancer.rate;
   }
   return total / freelancers.length;
@@ -33,30 +35,52 @@ function rateAverage(freelancers) {
 let rateaverage = rateAverage(freelancers);
 // === component ===
 // Write a component function to represent a single freelancer.
-function FreeLancerRow (freelancer) {
-    const {name, occupation, rate} = freelancer;
-    const $row = document.createElement("article");
-    $row.classList.add("row");
-    $row.innerHTML = `
-    <p class="freelancerinfo">${name} ${occupation} ${rate}</p>
+function FreeLancerRow(freelancer) {
+  const { name, occupation, rate } = freelancer;
+  const $row = document.createElement("tr");
+  $row.innerHTML = `
+    <td>${name}</td>
+    <td>${occupation}</td>
+    <td>${rate}</td>
     `;
-    return $row;
+  return $row;
 }
 //Write a component function to represent an array of freelancers.
-function FreeLancerRows () {
-    const $container = document.createElement("article");
-    $container.classList.add("rows")
-    const $rows = freelancers.map(FreeLancerRow);
-    $container.replaceChildren(...$rows);
-    return $container;
-
+function FreeLancerRows() {
+  const $table = document.createElement("table");
+  $table.classList.add("tfreelancers");
+  $table.innerHTML = `
+  <thead>
+    <tr>
+        <th>Name</th>
+        <th>Occupation</th>
+        <th>Rate</th>
+    </tr>
+  </thead>
+  <tbody></tbody>
+  `;
+  const $rows = freelancers.map(FreeLancerRow);
+  $table.replaceChildren(...$rows);
+  return $table;
 }
 //Write a component function to represent the average rate of all freelancers.
-function AverageRate () {
-    const $figcaption = document.createElement("article");
-    $figcaption.classList.add("text")
-    $figcaption.innerHTML = `
-    <p> The average is ${rateaverage}</p>
+function AverageRate() {
+  const $figcaption = document.createElement("p");
+  $figcaption.classList.add("text");
+  $figcaption.innerHTML = `
+    <p> The average rate is $${rateaverage}</p>
     `;
-    return $figcaption;
+  return $figcaption;
 }
+// === render ===
+function render() {
+  const $app = document.querySelector("#app");
+  $app.innerHTML = `
+    <h1>Freelancer Forum</h1>
+    <AverageRate></AverageRate>
+    <FreeLancerRows></FreeLancerRows>
+    `;
+  $app.querySelector("AverageRate").replaceWith(AverageRate());
+  $app.querySelector("FreeLancerRows").replaceWith(FreeLancerRows());
+}
+render();
